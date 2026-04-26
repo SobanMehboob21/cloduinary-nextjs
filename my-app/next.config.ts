01e4ebe69d7299bc4f013e4ebedacf7405ad1b67
Soban Mehboob <soban.mehboob@econceptions.mobi>
2026-04-26 15:19:21 +0500

    fix issues

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // <--- THIS IS REQUIRED
+  images: {
+    unoptimized: true, // Also required for static exports
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
